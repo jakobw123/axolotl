171c202a7fefe1c08ee134823d0ec48ad9c43fb7
Jakob <jw1193393@gmail.com>
2026-02-27 18:39:38 +0100

    Fix to vllm 0.13.0

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ def parse_requirements(extras_require_map):
                 if patch == 0:
                     extras_require_map["vllm"] = ["vllm==0.13.0"]
                 else:
-                    extras_require_map["vllm"] = ["vllm==0.14.0"]
+                    extras_require_map["vllm"] = ["vllm==0.13.0"]
             elif (major, minor) >= (2, 8):
                 extras_require_map.pop("fbgemm-gpu")
                 extras_require_map["fbgemm-gpu"] = ["fbgemm-gpu-genai==1.3.0"]
